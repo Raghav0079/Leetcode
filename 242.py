32e7d511a334741c1faacaf03f7045cffde9ae7c
Raghav Mishra <raghav051006@gmail.com>
2025-09-23 20:59:42 +0530

    Add files via upload

---
+++ 242.py
@@ -0,0 +1,22 @@
+''' Given two strings s and t, return true if t is an anagram of s, and false otherwise.'''
+
+class Solution(object):
+    def isAnagram(self,s,t):
+        """
+        :type s: str
+        :type t: str
+        :rtype: bool
+        """
+        if len(s) != len(t):
+            return False
+        count = {}
+        for char in s:
+            count[char] = count.get(char, 0) + 1
+        for char in t:
+            if char not in count:
+                return False
+            count[char] -= 1
+            if count[char] < 0:
+                return False
+        return True
+    
