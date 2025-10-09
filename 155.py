c320ac6b660d6f35003e1e009bd88bfbb87a3d95
Raghav Mishra <raghav051006@gmail.com>
2025-10-09 20:23:24 +0530

    Add files via upload

---
+++ 155.py
@@ -0,0 +1,56 @@
+'''Design a stack that supports push, pop, top, and retrieving the minimum element in constant time.
+
+Implement the MinStack class:
+
+MinStack() initializes the stack object.
+void push(int val) pushes the element val onto the stack.
+void pop() removes the element on the top of the stack.
+int top() gets the top element of the stack.
+int getMin() retrieves the minimum element in the stack.
+You must implement a solution with O(1) time complexity for each function.'''
+
+class MinStack(object):
+    def __init__(self):
+        """
+        initialize your data structure here.
+        """
+        self.stack = []
+        self.min_stack = []
+        
+
+    def push(self, val):
+        """
+        :type val: int
+        :rtype: None
+        """
+        self.stack.append(val)
+        if not self.min_stack or val <= self.min_stack[-1]:
+            self.min_stack.append(val)
+        
+
+    def pop(self):
+        """
+        :rtype: None
+        """
+        if self.stack:
+            val = self.stack.pop()
+            if val == self.min_stack[-1]:
+                self.min_stack.pop()
+        
+
+    def top(self):
+        """
+        :rtype: int
+        """
+        if self.stack:
+            return self.stack[-1]
+        
+
+    def getMin(self):
+        """
+        :rtype: int
+        """
+        if self.min_stack:
+            return self.min_stack[-1]
+        return None
+    
