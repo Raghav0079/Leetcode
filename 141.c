56b843dc0d2e55a1ef352dfbd3c3b6f6a0f916ae
Raghav Mishra <raghav051006@gmail.com>
2025-10-10 09:20:49 +0530

    Add files via upload

---
+++ 141.c
@@ -0,0 +1,25 @@
+/*Given head, the head of a linked list, determine if the linked list has a cycle in it.
+
+There is a cycle in a linked list if there is some node in the list that can be reached again by continuously following the next pointer. Internally, pos is used to denote the index of the node that tail's next pointer is connected to. Note that pos is not passed as a parameter.
+
+Return true if there is a cycle in the linked list. Otherwise, return false.*/
+
+#include <stdbool.h>
+#include <stdio.h>
+#include <stdlib.h>
+
+struct ListNode{
+    int val;
+    struct ListNode *next;
+};
+bool hasCycle(struct ListNode *head){
+    if(head == NULL || head->next == NULL) return false;
+    struct ListNode *slow = head;
+    struct ListNode *fast = head->next;
+    while(fast != NULL && fast->next != NULL){
+        if(slow == fast) return true;
+        slow = slow->next;
+        fast = fast->next->next;
+    }
+    return false;
+}
