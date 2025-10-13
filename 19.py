541233418b860dbdc89e8d54912706579cfc287e
Raghav Mishra <raghav051006@gmail.com>
2025-10-13 09:26:40 +0530

    Add files via upload

---
+++ 19.py
@@ -0,0 +1,21 @@
+'''Given the head of a linked list, remove the nth node from the end of the list and return its head.'''
+
+class Solution(object):
+    def removeNthFromEnd(self, head, n):
+        """
+        :type head: Optional[ListNode]
+        :type n: int
+        :rtype: Optional[ListNode]
+        """
+        dummy = ListNode(0)
+        dummy.next = head
+        first = dummy
+        second = dummy
+        for _ in range(n + 1):
+            first = first.next
+        while first:
+            first = first.next
+            second = second.next
+        second.next = second.next.next
+        return dummy.next
+    
