502d8951ff4677c5331689f9587fe033f26c84ab
Raghav Mishra <raghav051006@gmail.com>
2025-11-09 10:35:47 +0530

    Add files via upload

---
+++ 108.c
@@ -0,0 +1,26 @@
+/*Given an integer array nums where the elements are sorted in ascending order, convert it to a height-balanced binary search tree.*/
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <stdbool.h>
+
+/* Definition for a binary tree node. */
+struct TreeNode {
+    int val;
+    struct TreeNode *left;
+    struct TreeNode *right;
+};
+struct TreeNode* sortedArrayToBST(int* nums, int numsSize){
+    if (numsSize == 0) {
+        return NULL;
+    }
+    
+    int mid = numsSize / 2;
+    struct TreeNode* root = (struct TreeNode*)malloc(sizeof(struct TreeNode));
+    root->val = nums[mid];
+    
+    root->left = sortedArrayToBST(nums, mid);
+    root->right = sortedArrayToBST(nums + mid + 1, numsSize - mid - 1);
+    
+    return root;
+}
