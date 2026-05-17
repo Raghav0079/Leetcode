0b25d1ccdbfb1564b30dd3f1a4bc18667aa14919
Raghav Mishra <raghav051006@gmail.com>
2026-05-17 12:02:30 +0530

    update 1.py

--- 1.py
+++ 1.py
@@ -3,13 +3,18 @@
 You may assume that each input would have exactly one solution, and you may not use the same element twice.'''
 
 class Solution(object):
-    def twoSum(self,num,target):
+    def twoSum(self, nums, target):
         """
         :type nums: List[int]
         :type target: int
         :rtype: List[int]
         """
-        for i in range(len(num)):
-            for j in range(i+1,len(num)):
-                if num[i]+num[j]==target:
-                    return [i,j]
+        prevmap = {}
+
+        for i , n in enumerate(nums):
+            diff = target - n 
+            if diff in prevmap:
+                return [prevmap[diff],i]
+            prevmap[n]=i
+        return 
+        
